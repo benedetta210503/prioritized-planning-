49cdfe9d391344009fd51423ead730eaf2314c24
benedetta210503 <benedetta.conti2105@gmail.com>
2025-07-01 22:48:53 +0200

    Update genera_grafici.py

--- genera_grafici.py
+++ genera_grafici.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from typing import List
 from agente import *
-def genera_grafici(agents: List[Agent], expanded_nodes: List[int], running_time: List[float], costo: List[int])->None:
+def genera_grafici(agents: List[Agent], expanded_nodes: List[int], running_time: List[float], costo: List[int],map_name:str)->None:
     """
         Questa funzione genera tre grafici che evidenziano l'andamento delle prestazioni 
         dell'algoritmo Prioritizied Planning al variare del numero numero di agenti.
@@ -15,6 +15,7 @@ def genera_grafici(agents: List[Agent], expanded_nodes: List[int], running_time:
         -)expanded_nodes (List[int]): Lista del numero di nodi espansi in ciascun esperimento.
         -)running_time (List[float]): Lista dei tempi di esecuzione in secondi.
         -)costo (List[int]): Lista dei costi totali dei percorsi generati.
+        -)map_name: nome della mappa che si intende testare
 
 
     """
@@ -45,19 +46,20 @@ def genera_grafici(agents: List[Agent], expanded_nodes: List[int], running_time:
 
    
     plt.tight_layout()
-    plt.savefig("grafici_risultati.png")
+    plt.savefig(f"grafici_risultati_{map_name}.png")
 
     plt.close()
 
-def genera_percentuali(num_agents:int,num_failure:int,num_success:int):
+def genera_percentuali(num_agents:int,num_failure:int,num_success:int,map_name:str):
     """
         Questa funzione genera un grafico a barre che mostra il conteggio dei successi e fallimenti 
         degli esperimenti per un dato numero di agenti.
 
         Gli argomenti della funzione sono:
-        -)il numero degli agenti
-        -)il numero di fallimenti, cioè di volte in cui PP non è in grado di trovare una soluzione valida
-        -)il numero di successi di PP
+        -)num_agent:il numero degli agenti
+        -)num_failure:il numero di fallimenti, cioè di volte in cui PP non è in grado di trovare una soluzione valida
+        -)num_success:il numero di successi di PP
+        -)map_name:nome della mappa che si intende testare
     """
     labels = ['Successi', 'Fallimenti']
     values = [num_success, num_failure]
@@ -69,5 +71,5 @@ def genera_percentuali(num_agents:int,num_failure:int,num_success:int):
     plt.ylabel('Numero Esperimenti')
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.tight_layout()
-    plt.savefig("percentuali_successi.png")
+    plt.savefig(f"percentuali_successi_{map_name}.png")
     plt.close()
